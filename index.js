707743a93368e44842b56a4a92d833b29f24a05f
Thaís Rodeiro <opabby82@gmail.com>
2022-12-29 19:17:31 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log('What is the capital of Peru?')  
+console.log('Lima!')
+console.log('Ready for next question?')
+
+/*
+Challenge:
+1. Make the third console.log fire 3 seconds after 
+   the answer appears.
+*/
