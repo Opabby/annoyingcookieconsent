f3d5b1de82f0caf7534615a8d8a522af38f67f5d
Thaís Rodeiro <opabby82@gmail.com>
2023-03-24 10:46:28 -0300

    adding modal's final events

--- index.js
+++ index.js
@@ -1,5 +1,46 @@
 const modal = document.querySelector('.modal');
+const closeBtn = document.querySelector('#modal-close-btn');
+const modalForm = document.querySelector('#modal-form');
+const modalText = document.querySelector('#modal-text');
+const modalInner = document.querySelector('#modal-inner');
+const modalBtns = document.querySelector('.modal-choice-btns');
+const declineBtn = document.querySelector('.decline');
 
 setTimeout(function() {
    modal.style.display = 'block';
 }, 3000);
+
+closeBtn.addEventListener("click", function() {
+   modal.style.display = 'none';
+})
+
+
+declineBtn.addEventListener('mouseenter', function(){
+   modalBtns.classList.toggle('reverse');
+})
+
+modalForm.addEventListener('submit', function(e){
+   e.preventDefault();
+   const modalFormData = new FormData(modalForm);
+   console.log(modalFormData);
+   const userName = modalFormData.get('userName');
+   modalText.innerHTML = `<div class="modal-inner-loading">
+                              <img src="images/loading.svg" class="loading">
+                              <p id="uploadText">
+                                 Uploading your data to the dark web...
+                              </p>
+                           </div>`;
+   setTimeout(function() {
+      document.querySelector('#uploadText').innerText = `Making the sale...`;
+   }, 2500);
+   setTimeout(function(){
+      modalInner.innerHTML = `<h2>Thanks <span class='modal-display-name'>${userName}</span>, you sucker! </h2>
+                              <p>We just sold the rights to your eternal soul.</p>
+                              <div class="idiot-gif">
+                                 <img src="images/pirate.gif">
+                              </div>
+                              `
+      closeBtn.disabled = false;
+   }, 5000);
+})
+
